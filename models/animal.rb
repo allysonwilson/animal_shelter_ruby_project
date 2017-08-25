83da65c1e462c79c1aa50c1e40d47e6d9f6cd2bb
Allyson Gayle Wilson <allysonmacwilson@gmail.com>
2017-08-25 14:03:03 +0100

    models with CRUD

--- models/animal.rb
+++ models/animal.rb
@@ -11,5 +11,41 @@ def initialize( options )
   @adoptable =  options['adoptable'] #boolean
 end
 
+def save()
+  sql = "INSERT INTO animals
+  (
+    name,
+    run_speed
+  )
+  VALUES
+  (
+    $1, $2
+  )
+  RETURNING id"
+  values = [@name, @run_speed]
+  results = SqlRunner.run(sql, values)
+  @id = results.first()['id'].to_i
+end
+
+def self.all()
+  sql = "SELECT * FROM animals"
+  values = []
+  results = SqlRunner.run( sql, values )
+  return results.map { |animal| Animal.new( animal ) }
+end
+
+def self.find( id )
+  sql = "SELECT * FROM animals
+  WHERE id = $1"
+  values = [id]
+  results = SqlRunner.run( sql, values )
+  return Animal.new( results.first )
+end
+
+def self.delete_all
+  sql = "DELETE FROM animals"
+  values = []
+  SqlRunner.run( sql, values )
+end
 
 end
