22029241c3a8842442f20254049deb16b0def094
Allyson Gayle Wilson <allysonmacwilson@gmail.com>
2017-08-29 13:38:01 +0100

    some more css

--- controllers/hero_controller.rb
+++ controllers/hero_controller.rb
@@ -6,3 +6,19 @@ get '/heroes' do
   @heroes = Hero.all()
   erb ( :"heroes/index" )
 end
+
+get '/heroes/new' do
+  @heroes = Hero.all()
+  erb ( :"heroes/new" )
+end
+
+post '/heroes/:id' do
+  hero = Hero.new(params)
+  hero.update
+  redirect to "/heroes/#{params['id']}"
+end
+
+post '/heroes' do
+  Hero.new(params).save
+  redirect to '/heroes'
+end
